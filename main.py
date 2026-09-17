030dbde3bb3667893f5c8b0c4527e5c17b632ec7
Juan Romero <juromerod@cuc.edu.co>
2026-09-16 22:28:04 -0500

    feat: agregar menu principal del sistema

---
+++ main.py
@@ -0,0 +1,8 @@
+canchas = ["Cancha 1", "Cancha 2", "Cancha 3"]
+
+def mostrar_menu():
+    print("=== CLUB DE TENIS ===")
+    print("1. Ver canchas disponibles")
+    print("2. Salir")
+
+mostrar_menu()
